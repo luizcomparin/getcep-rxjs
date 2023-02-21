9303999cbf0cf8dbf160b82c8b1a0ed5b61986d4
Luiz Comparin <luizcomparin18@gmail.com>
2023-02-21 20:12:28 -0300

    PrimeNG Material config

---
+++ src/app/primeng-modules.ts
@@ -0,0 +1,9 @@
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+
+export const PrimeNgMaterialsModule = [
+	InputTextModule,
+	ButtonModule,
+	TooltipModule,
+];
